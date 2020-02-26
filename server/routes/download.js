039cf78a217606c45306fbbb77011fc824ec3154
Benjamin Vera <ben@soundcontrolrecords.com>
2020-02-26 15:56:20 -0600

    remove auth from dl order ep

--- server/routes/download.js
+++ server/routes/download.js
@@ -8,11 +8,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 //protects routes by verifying user token
 const downloadController = require('../controllers/download');
 
-router.get(
-  '/order/:orderId/:product_id',
-  requireAuth,
-  downloadController.downloadFromOrder,
-);
+router.get('/order/:orderId/:product_id', downloadController.downloadFromOrder);
 router.get('/:product_id', requireAuth, downloadController.downloadFromProfile);
 
 module.exports = router;
