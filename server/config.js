6354f456b636dbed138b6c031e1cf7765e7cad39
Benjamin Vera <ben@soundcontrolrecords.com>
2020-03-17 18:40:57 -0500

    mongodb atlas

--- server/config.js
+++ server/config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  secret: "littlesecret",
-  dbUri: "mongodb://localhost:27017/ET3",
+  secret: 'littlesecret',
+  dbUri:
+    'mongodb+srv://ben:pacers@cluster0-jxib3.mongodb.net/ET3?retryWrites=true&w=majority',
   port: 3090,
   PAYPAL_CLIENT_ID:
-    "AakM4NG8lS-m62tcOiex6PvYOqTeZ3aogMQAwPsA8wvHZ315fDyTxremwedbssYSai1hWyTayKlFOatL",
+    'AakM4NG8lS-m62tcOiex6PvYOqTeZ3aogMQAwPsA8wvHZ315fDyTxremwedbssYSai1hWyTayKlFOatL',
   PAYPAL_CLIENT_SECRET:
-    "EDEq95TXcPGNYGFwMlPTF3bK_LZKSDjBIsmI5XJFMLczM7ajG51TqPQNUbACQHDYYnlRjOY02J2oKtsD"
+    'EDEq95TXcPGNYGFwMlPTF3bK_LZKSDjBIsmI5XJFMLczM7ajG51TqPQNUbACQHDYYnlRjOY02J2oKtsD',
 };
