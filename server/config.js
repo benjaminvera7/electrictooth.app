b1496c64df15229e774e29b67a254d9bbae680ee
Benjamin Vera <ben@soundcontrolrecords.com>
2020-03-16 19:18:08 -0500

    adding config

---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  secret: "littlesecret",
+  dbUri: "mongodb://localhost:27017/ET3",
+  port: 3090,
+  PAYPAL_CLIENT_ID:
+    "AakM4NG8lS-m62tcOiex6PvYOqTeZ3aogMQAwPsA8wvHZ315fDyTxremwedbssYSai1hWyTayKlFOatL",
+  PAYPAL_CLIENT_SECRET:
+    "EDEq95TXcPGNYGFwMlPTF3bK_LZKSDjBIsmI5XJFMLczM7ajG51TqPQNUbACQHDYYnlRjOY02J2oKtsD"
+};
---
+++ server/key.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'service_account',
+  project_id: 'electric-tooth',
+  private_key_id: 'ff22fc9c2087bf4e6a74fc5a42cfe272a0c48b32',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDO0s5bRz7ypG/F\nQUly8Y7cwN6sObJxoxohx9NE4j/XrF4FCSGzc0rkPqsVp+ONChQL5ZDaqAKAwDZd\nhRCVsuuLWv0vZhRTlq070FiMKyXtrb73OjB6N0DJDsfh7UTuoVxcQRVRn4oKBCWM\nidoPIdXF1xNDWQkf19023DmfU81O5F8cNCWxC/DbFLAJ61sTYyhNgT+sV8+wT5JQ\nPRSITVqOMNTA7kXw46fQiU4XBJNqRp2mXtuTODIR30mNN0hlpSXrkFbik0phBpTr\nL+ogQTXYNHQx1aUwySKCKzYpiWyCACrlrDsR1cOwXe6NUe12ihzBScILMymuFxxB\n9vlhkcuNAgMBAAECggEAAU1PZc/buk7sTocN3cglqfqGPSWj8FY7WzZ1BOmYWn8Q\nJTzvo7TorscAoA4l5QVGvQwhEDk4SR5yEqkia9J+WJ3W+nIlfyvBohY2KkDzLo5+\nuxYFYMQPCtT7HaPO77+SFEkA+bfHn4n+UC5oDCkAGv9VgZVG3R1LJurPrIbz7QN3\nqi8x3Z2VeGXoK/1q2VH8wcKQY/fZdbSbAFUJqh6uISNw0O7oDtqZRzIHon6voxiS\n2JbpcUTGAVySUKIk1xhRDtKmVPbaCOxGH5opM2XMcUZDbIUb7+ivN/JEQGVGM68C\n/55XEiDYmQwSgromqTyDRjtckYov8bpaZoWP8Nm1zwKBgQD41EvslqRuAnjlttVO\nq/ae3N0pWDjXNkC/LCNrKvyUhgVtiQeKlAKiBxl0ovEVOqCwyQ+IvSpqW6zKNTM/\nLZoYFerZ2TyNDgROpHDK46vBh4dmzFVk8ucuFpjxTMRcdhUd45zu5yvApoPRi5Fy\nRxBIRX6vm1CUUW/z28ERNsKzowKBgQDUyJ4NYsmXlhys0enQYL366ZJUmy7NJj59\ntTisTQa/UaPV4La7fY7ZoJ5cdC/0R7zzkAHqDbJlYhegl6iiGpO7pZt5kEC+Zm0f\n+1znp5hIYISyRlbvZuhes/FjaIe8PH1skFGEIavJH9kStCLI5a+PYH2ovo6CgUMh\nMElhoT33DwKBgGlvu8RCiOY8kfH0qSou38+tEuHECkHXMVRQZamxi1l7+KzTVai4\n0uzJSVYM/U7bit+eMsDkNybeNbdxF4f70MHH973buTaj69tO/qRaGJtmzE8Y4k5m\nRuNWm/QTbVkoiAYkPolU10tIw651D2p3BkU5fVb33wsZ/vA7+/FJXPM1AoGBAK+y\n9Cczcih0MDtXnYyNxXGBGm44MV3+fU1JrYFAnRivLex2AA0y8Riscvb+jXZVIiWv\nvJTqOGtUoxPdBsbL0T750TaAsaVGaEcnRKCYF7tzzz5xrGlD/Q3t4Z1m++WnwR06\n0cScFQAfBxxVl/9xARUP5NLIfl4upCKsf3tZLBRrAoGAHlMBUUcfMvLqxNqAYMAl\nIo4NjFM3f9MAZy+83bTTpLa62X1RFoBO1NHYP4UaXbxPmhwO8zQ1vy7TRFaEIkHV\nP2viwQkVr7F4BzOeMyyMlxGtGk5Jhxt0BTE57ud7S0YXhs8Dg4qSpWAB5rwB3Blf\neDiMhsBTwJrnDrWNb76vFrg=\n-----END PRIVATE KEY-----\n',
+  client_email: 'et-emails@electric-tooth.iam.gserviceaccount.com',
+  client_id: '115626705788788185221',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/et-emails%40electric-tooth.iam.gserviceaccount.com',
+};
