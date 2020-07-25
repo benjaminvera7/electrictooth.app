15e4058ef303f8efbf601012f1266e05b361d4c7
Benjamin Vera <contact@benjaminvera.com>
2020-07-25 14:50:54 -0500

    added song permission expire date to session

--- server/controllers/stream.js
+++ server/controllers/stream.js
@@ -2,23 +2,46 @@ const path = require('path');
 const fs = require('fs');
 const dbConnection = require('../services/database');
 
+async function userOwnsProduct(user, song) {
+  const albumCollection = await user.getAlbumCollection();
+
+  let songExists = albumCollection.filter(
+    (item) => item.product_id === song.product_id,
+  );
+
+  let albumExists = albumCollection.filter(
+    (item) => item.id.toString() === song.album.toString(),
+  );
+
+  return !(songExists.length === 0 && albumExists.length === 0);
+}
+
 async function stream(req, res) {
-  const id = req.params.songId;
+  const { user, params, session } = req;
+
+  const id = params.songId;
   const song = await dbConnection.getSongById(id);
+  const owns = await userOwnsProduct(user, song);
 
-  // if (req.session.stream.id !== req.params.songId) {
-  //   await dbConnection.subtractCoin(req.user);
+  if (!owns) {
+    if (!session.stream) {
+      session.stream = {
+        id: null,
+        expire: null,
+      };
+    }
 
-  //   req.session.stream = {
-  //     id: req.params.songId,
-  //     expire: Date.now() + 60000, //1 minute permission
-  //     owned: true,
-  //   };
-  // }
-
-  // req.session.save((err) => {
-  //   console.log(err, 'save');
-  // });
+    if (
+      Date.now() > session.stream.expire ||
+      session.stream.expire === null ||
+      session.stream.id !== id
+    ) {
+      await dbConnection.subtractCoin(user);
+      session.stream.id = id;
+      session.stream.expire = Date.now() + 60000; 
+      session.save();
+    }
+  }
 
   const music = path.join(__dirname, `../music/${song.stream_url}`);
   const stat = fs.statSync(music);
