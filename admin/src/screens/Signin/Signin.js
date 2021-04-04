5a14e698fd381139ced2e7b976047b8b5390c4ed
Benjamin Vera <contact@benjaminvera.com>
2021-04-04 16:23:58 -0500

    fuuu

--- admin/src/screens/Signin/Signin.js
+++ admin/src/screens/Signin/Signin.js
@@ -25,7 +25,7 @@ const Signin = ({ history }) => {
         formData.append('password', admin.password);
 
         axios
-            .post('http://eletrictooth.app/api/v1/admin/signin', formData, {
+            .post('https://eletrictooth.app/api/v1/admin/signin', formData, {
                 headers: {
                     'content-type': 'multipart/form-data',
                 },
