24300ddf5e1b6c21adbc43ca2a8013af21043e55
Benjamin Vera <contact@benjaminvera.com>
2021-09-25 12:30:02 -0500

    adding social to artist schema

--- server/models/artists.js
+++ server/models/artists.js
@@ -6,6 +6,14 @@ const ArtistsSchema = new Schema({
   artist_bio: String,
   artist_img: String,
   created_at: Date,
+  facebook: String,
+  twitter: String,
+  soundcloud: String,
+  bandcamp: String,
+  tiktok: String,
+  instagram: String,
+  spotify: String,
+  applemusic: String,
   albums: [{ type: Schema.Types.ObjectId, ref: 'Albums' }],
 });
 
